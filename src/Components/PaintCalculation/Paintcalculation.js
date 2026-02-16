096d581f8b133fabc4cc4088d784ccfe4f16d6d4
pandikuma <pandeeswaran12022002@gmailcom>
2026-02-16 10:49:16 +0530

    Updated

--- src/Components/PaintCalculation/Paintcalculation.js
+++ src/Components/PaintCalculation/Paintcalculation.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react"; 
 import Select from "react-select";
 import deleteIcon from '../Images/Worng.svg';
 import deletes from '../Images/Delete.svg';
